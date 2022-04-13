49a1b2243818d521829dab8b73cb4a279018e09a
Gutovp1 <paivaav@gmail.com>
2022-04-12 22:26:45 -0300

    Create play round function

--- javascript.js
+++ javascript.js
@@ -5,7 +5,23 @@ function computerPlay(){
     return pcplay[index];
 }
 
+function playRound(playerSelection,computerSelection){
+    if(playerSelection === computerSelection){
+        result = 'Draw. Play again';
+    }
+    else {
+        if((playerSelection == 'rocket' && computerSelection == 'scissor') || (playerSelection == 'scissor' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'rocket')){
+            result = "You won. Because, "+ playerSelection +" beats "+ computerSelection +".";
+        }else{
+            result = "You lost. Because, "+ playerSelection +" is beaten by "+ computerSelection +".";
+        }
+        
+    }
+    return result;
+}
+
 const playerSelection = 'rocket';
 const computerSelection = computerPlay();
 console.log(playerSelection);
 console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection));
