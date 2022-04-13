a707af58ded88c3ce7cb813af4f3fcb0a704dc02
Gutovp1 <paivaav@gmail.com>
2022-04-12 22:33:31 -0300

    Create player play function and rock name correction

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 
 function computerPlay(){
-    let pcplay = ['rocket','scissor','paper'];
+    let pcplay = ['rock','scissor','paper'];
     let index = Math.floor((Math.random()*10))%3;
     return pcplay[index];
 }
@@ -10,7 +10,7 @@ function playRound(playerSelection,computerSelection){
         result = 'Draw. Play again';
     }
     else {
-        if((playerSelection == 'rocket' && computerSelection == 'scissor') || (playerSelection == 'scissor' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'rocket')){
+        if((playerSelection == 'rock' && computerSelection == 'scissor') || (playerSelection == 'scissor' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'rock')){
             result = "You won. Because, "+ playerSelection +" beats "+ computerSelection +".";
         }else{
             result = "You lost. Because, "+ playerSelection +" is beaten by "+ computerSelection +".";
@@ -20,7 +20,23 @@ function playRound(playerSelection,computerSelection){
     return result;
 }
 
-const playerSelection = 'rocket';
+function playerPlay(){
+    let validAnswer = false;
+    while(!validAnswer){
+        response = prompt("Write your answer down: paper, rock or scissor.");
+        lower_response = response.toLowerCase();
+        if(lower_response == "rock" || lower_response == "scissor" || lower_response == "paper"){
+          validAnswer=true;
+        }else {    
+            response = prompt("Write a valid choice: rock, paper or scissor.");
+            lower_response = response.toLowerCase();
+        }
+    }
+    return lower_response;
+}
+
+//const playerSelection = 'rock';
+let playerSelection = playerPlay();
 const computerSelection = computerPlay();
 console.log(playerSelection);
 console.log(computerSelection);
